9696d05eb26723114e5f992579be627dad570f15
G Jagadeep <131535934+jagadeep18@users.noreply.github.com>
2024-05-31 16:45:04 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='secret_vault',
+    version='1.1',
+    description='Python secret vault to encrypt and hide your files in a secure place.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Maneesh Pradeep',
+    author_email='maneesh.pradeep@protonmail.com',
+    url='https://github.com/Maneesh-Pradeep/secret_vault',
+    license='MIT',
+    py_modules=['secret_vault'],
+    install_requires=['cryptography', 'pyAesCrypt'],
+    entry_points={
+        'console_scripts': [
+            'secret_vault = secret_vault:main',
+        ],
+    },
+    classifiers=[
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+    ],
+)
